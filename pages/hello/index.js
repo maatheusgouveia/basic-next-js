adf6b5fee5f92709792e82526265994d7267b07f
maatheusgouveia <matheus_guitarplayer@hotmail.com>
2020-08-25 00:44:55 -0300

    tela inicial e navegação

---
+++ pages/hello/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+
+function Hello() {
+	return (
+		<div>
+			<Typography align="center">Hello NextJS</Typography>
+		</div>
+	);
+}
+
+export default Hello;
