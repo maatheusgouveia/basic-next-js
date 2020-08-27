8f86fb3bc5a088f5605ec0298ee78110948d999c
maatheusgouveia <matheus_guitarplayer@hotmail.com>
2020-08-26 21:43:57 -0300

    alterações no layout e rotas

--- pages/hello/index.js
+++ pages/hello/index.js
@@ -1,10 +1,58 @@
-import React from "react";
-import { Typography } from "@material-ui/core";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { Typography, Paper, Grid, Button } from "@material-ui/core";
+
+import styles from "../../styles/Home.module.css";
 
 function Hello() {
+	const router = useRouter();
+
 	return (
-		<div>
-			<Typography align="center">Hello NextJS</Typography>
+		<div className={styles.container}>
+			<Head>
+				<title>Estudo de NextJS</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<header className={styles.header}>
+				<Typography
+					component="h1"
+					variant="h3"
+					className={styles.title}
+				>
+					NextJS
+				</Typography>
+			</header>
+
+			<main className={styles.content}>
+				<Paper className={styles.paper}>
+					<Typography
+						className={styles.subtitle}
+						align="center"
+						variant="h5"
+						component="h1"
+					>
+						Outra página
+					</Typography>
+
+					<Grid
+						container
+						alignItems="center"
+						justify="center"
+						direction="column"
+					>
+						<Typography variant="caption" align="center">
+							Utilizando Material design e NextJS para aprender os
+							conceitos básicos do framework
+						</Typography>
+
+						<Button onClick={() => router.push("/")}>Voltar</Button>
+					</Grid>
+				</Paper>
+			</main>
+
+			<footer className={styles.footer}>
+				<Typography align="center">Matheus Gouveia - 2020</Typography>
+			</footer>
 		</div>
 	);
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from "next/head";
-import Link from "next/link";
-import { Paper, Typography, Button } from "@material-ui/core";
+import { useRouter } from "next/router";
+import { Paper, Typography, Button, Grid } from "@material-ui/core";
 
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+	const router = useRouter();
+
 	return (
 		<div className={styles.container}>
 			<Head>
@@ -12,26 +14,46 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<header className={styles.header}>
-				<Typography align="center" component="h1" variant="h3">
+				<Typography
+					component="h1"
+					variant="h3"
+					className={styles.title}
+				>
 					NextJS
 				</Typography>
 			</header>
 
 			<main className={styles.content}>
 				<Paper className={styles.paper}>
-					<Typography variant="caption">
-						Utilizando Material design e NextJS para aprender os
-						conceitos básicos
+					<Typography
+						className={styles.subtitle}
+						align="center"
+						variant="h5"
+						component="h1"
+					>
+						Página inicial
 					</Typography>
 
-					<Link href="/hello">
-						<Button>Navegação</Button>
-					</Link>
+					<Grid
+						container
+						align="center"
+						direction="column"
+						alignItems="center"
+					>
+						<Typography variant="caption" align="center">
+							Utilizando Material design e NextJS para aprender os
+							conceitos básicos do framework
+						</Typography>
+
+						<Button onClick={() => router.push("/hello")}>
+							Navegação
+						</Button>
+					</Grid>
 				</Paper>
 			</main>
 
 			<footer className={styles.footer}>
-				<Typography align="center">NextJS</Typography>
+				<Typography align="center">Matheus Gouveia - 2020</Typography>
 			</footer>
 		</div>
 	);
